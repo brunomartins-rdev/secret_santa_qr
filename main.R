2c5534d32be93e9edf9935b3af3bdcfdc00c9ad7
Bruno Martins <bruno.silva.martins@hotmail.com>
2025-01-16 19:02:09 +0000

    Initial commit

---
+++ main.R
@@ -0,0 +1,19 @@
+# Libraries ---------------------------------------------------------------
+library(qrcode)
+library(ggplot2)
+
+# Users -------------------------------------------------------------------
+# Include here the names of the secret Santa people
+givers <- c("Jon Doe", "Joane Doe", "Doe Jon", "Doe Joane")
+
+takers <- givers
+
+# Attribution logic ------------------------------------------------------
+for (i in givers) {
+  j <- sample(takers[takers != i], 1)
+  takers <- takers[takers != j]
+  code <- qr_code(as.character(j))
+  png(filename = paste0(i, ".png"))
+  plot(code)
+  dev.off()
+}
